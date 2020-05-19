d3bbc103c3a9b267f49fde1524c8ec827a59e7e6
Tyrone Lagore <tyronelagore@gmail.com>
2020-05-18 18:29:01 -0700

    formatting

--- src/components/Deck/DeckList.vue
+++ src/components/Deck/DeckList.vue
@@ -1,47 +1,52 @@
 <template>
-  <div class="container">
-    <div class="row" v-for="(deck, deckName) in decks" :key="deckName">
-      <div class="col-lg-3 col-sm-12">
+  <div class="row p-2">
+    <div class="col-lg-3 col-sm-12 p-1 cardBackground" v-for="(deck, deckName) in decks" :key="deckName">
         <b-card
           :title="deckName"
           :sub-title="deck.description"
+          class="deck"
         >
           <div class="d-flex justify-content-around">
             <div>
-              <a href="#"  class="m-auto">
-                <b-icon-pencil variant="warning"></b-icon-pencil>
-              </a>
+              <p class="h1" style="margin-top:-5px;">
+                <a href="#"  class="m-auto">
+                  <b-icon-play-fill variant="success"></b-icon-play-fill>
+                </a>
+              </p>
             </div>
             <div>
-              <div  v-if="deck.confirmDelete" class="d-flex flex-column">
-                <h6>Confirm?</h6>
+              <p class="h3">
+                <a href="#"  class="m-auto">
+                  <b-icon-pencil variant="warning"></b-icon-pencil>
+                </a>
+              </p>
+            </div>
+            <div>
+              <!-- if we haven't tried to delete the deck yet, this will be undefined / false -->
+              <div v-if="deck.confirmDelete" class="d-flex flex-column">
+                <small>Delete?</small>
                 <div class="d-flex">
-                  <a v-on:click="deleteDeck(deckName)" href="#" class="m-auto">
-                    <b-icon-check variant="success"></b-icon-check>
-                  </a>
-                  <a v-on:click="deleteCancel(deckName)" href="#" class="m-auto">
-                    <b-icon-x variant="danger"></b-icon-x>
-                  </a>
+                  <p class="h4">
+                    <a v-on:click="deleteDeck(deckName)" href="#" class="m-auto">
+                      <b-icon-check variant="success"></b-icon-check>
+                    </a>
+                  </p>
+                  <p class="h4">
+                    <a v-on:click="deleteCancel(deckName)" href="#" class="m-auto">
+                      <b-icon-x variant="danger"></b-icon-x>
+                    </a>
+                  </p>
                 </div>
               </div>
-              <a v-else v-on:click="deleteConfirm(deckName)" href="#" class="m-auto">
-                <b-icon-trash variant="danger"></b-icon-trash>
-              </a>
+              <p v-else class="h3">
+                <a v-on:click="deleteConfirm(deckName)" href="#" class="m-auto">
+                  <b-icon-trash variant="danger"></b-icon-trash>
+                </a>
+              </p>
             </div>
           </div>
         </b-card>
       </div>
-      <!-- <div class="d-inline-flex p-2 align-items-center justify-content-center border">
-        <div>
-          <h3>
-            {{ deckName }}
-          </h3>
-          <h6>
-            {{ value.description}}
-          </h6>
-        </div>
-      </div> -->
-    </div>
   </div>
 </template>
 
@@ -65,11 +70,13 @@ export default {
   },
   beforeMount() {
     this.decks = this.$store.getters.decks;
-
-    // add a custom delete check per deck
-    for(const [,deck] in this.decks) {
-      this.decks[deck.deckName].confirmDelete = false;
-    }
   }
 }
 </script>
+
+
+<style>
+  .cardBackground {
+    background-color: cornflowerblue
+  }
+</style>
